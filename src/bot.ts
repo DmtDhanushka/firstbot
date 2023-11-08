35069176f0dc3c3133574ebbed174390ca343031
Dimuthu.D <Dimuthu.D@creativesoftware.com>
2023-11-08 12:38:53 +0530

    Welcome choice

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { ActivityHandler, MessageFactory } from 'botbuilder';
+import { ActionTypes, ActivityHandler, MessageFactory, TurnContext } from 'botbuilder';
 
 export class EchoBot extends ActivityHandler {
     constructor() {
@@ -15,15 +15,26 @@ export class EchoBot extends ActivityHandler {
         });
 
         this.onMembersAdded(async (context, next) => {
-            const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
-            for (const member of membersAdded) {
-                if (member.id !== context.activity.recipient.id) {
-                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-                }
-            }
+            await this.sendWelcomeMessage(context);
             // By calling next() you ensure that the next BotHandler is run.
             await next();
         });
+    }
+
+    async sendWelcomeMessage(context: TurnContext) {
+        const membersAdded = context.activity.membersAdded;
+        const welcomeText = 'Hey there!';
+        for (const member of membersAdded) {
+            if (member.id !== context.activity.recipient.id) {
+                await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                await this.sendSuggestedActions(context)
+            }
+        }
+    }
+
+    async sendSuggestedActions(turnContext) {
+        const cardActions = ['Book reservation', 'know more info'];
+        var reply = MessageFactory.suggestedActions(cardActions, 'How can I help you?');
+        await turnContext.sendActivity(reply);
     }
 }
